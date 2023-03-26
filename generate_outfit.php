fbc2d196c1ec5ded86bda02c346e64dab42459f3
monicadumescu <65350499+monicadumescu@users.noreply.github.com>
2023-03-26 16:03:44 +0300

    Update generate_outfit.php

--- generate_outfit.php
+++ generate_outfit.php
@@ -74,78 +74,117 @@ include('session.php');
       <div class="col-sm-12 col-lg-6 mt-3">
 
         <?php
-        $host = 'localhost';
-        $user = 'root';
-        $pass = '';
-        $database = 'products';
+       $host = 'localhost';
+       $user = 'root';
+       $pass = '';
+       $database = 'products';
+
+
+
+
+        $clothesMap = array(
+          "hoodie" => "casual",
+          "coat" => "casual",
+          "t-shirt" => "casual",
+          "jeans" => "casual",
+          "swimsuit" => "sport",
+          "skirt" => "casual",
+          "jacket" => "casual",
+          "trainers" => "casual",
+          "hat" => "casual",
+          "backpack" => "casual",
+          "shorts" => "sport",
+          "gumshoes" => "sport",
+          "cap" => "sport",
+          "mayow" => "sport",
+          "flip-flop" => "sport",
+          "fanny pack" => "sport",
+          "dress" => "elegant",
+          "sweater" => "elegant",
+          "oxford shoes" => "elegant",
+          "suit" => "elegant",
+          "high heels" => "elegant",
+          "tie" => "elegant",
+          "overcoat" => "elegant",
+          "polo shirt" => "elegant",
+          "shirt" => "elegant",
+          "pants" => "elegant",
+          "purse" => "elegant"
+        );
+
+
 
         $conn = new mysqli($host, $user, $pass, $database);
         if ($conn->connect_error) {
           die("Connection failed: " . $conn->connect_error);
         }
-        $sql_out = "select * from ProductsTable where ";
-        // dupa facem un fel de filtru frumos in functie de ce a ales clientul bazat pe urmatoarele criterii:
-//eveniment
-//anotimp
-//preferinte
-//buget
+        $sql_out = "select distinct * from productstable where ";
+
+        //filtram in functie de
+        //eveniment
+        //anotimp
+        //preferinte
         
-        $eveniment = "wedding";
-        $anotimp = "winter";
-        $preferinte = $_GET['preferinte'];
+        $gen = "woman";
+        $eveniment = "pool party";
+        $anotimp = "summer";
+        $preferinte = "sport";
         $buget = 500;
         $total_price = 0;
 
         $sql_out = $sql_out . "(name like '%{$eveniment}%' or description like '%{$eveniment}%') AND (name like '%{$anotimp}%' or description like '%{$anotimp}%')";
 
 
-        switch ($preferinte) {
-          case "formal":
-            //do smth
-            break;
-          case "casual":
-            //do smth
-            break;
-          case "sport":
-            // do smth
-            break;
-          default:
-            break;
-        }
-
         $result_out = mysqli_query($conn, $sql_out);
+
         if ($result_out) {
           $count = mysqli_num_rows($result_out);
 
-          if ($count > 0) {
-            while ($row_out = $result_out->fetch_assoc()) {
-              $total_price = $total_price + $row_out['price'];
+          //le afisam in functie de pret si de tip ca s rezulte un outfit
+          /*Tipuri de produse: 
+          1 - bluze, tricouri, etc.
+          2 - pantaloni, fuste
+          3 - pantofi
+          4 - rochii
+          5 - chestii de luat pe deasupra: geci, paltoane, etc.
+          6 - chetii pt tinut chestii: genti, ghizdane, etc.
+          7 - chetii de pus pe cap
+          */
+
+          while ($row = $result_out->fetch_assoc()) {
+            $result_out2It = mysqli_query($conn, $sql_out);
+
+            foreach ($clothesMap as $key => $value) {
+
+              if ($gen != "woman" && ($row["name"] == "skirt" || $row["name"] == "dress" || $row["name"] == "purse" || $row["name"] == "swimsuit")) {
+                continue;
+              }
+
+              if ($key == $row["name"] && $value == $preferinte && $row["price"] < $buget) {
+
+                $s = $row['files']; ?>
+                <div class='div2'>
+                  <style>
+                    .div2 {
+                      border-style: groove;
+                    }
+                  </style><img src="<?php echo $s; ?>" alt="HTML5 Icon" style="width:500px;height:500px">
+                  <br>Category:
+                  <?php echo $row["name"]; ?> <br>Price:
+                  <?php echo $row['price']; ?>
+                  <br>Size:
+                  <?php echo $row['si_ze']; ?>
+                  <br>Description:
+                  <?php echo $row['description']; ?>
+                  <br><a class="btn btn-outline-success my-2 my-sm-1" href="add_chart.php?id=<?php echo $row['id']; ?>"
+                    type="submit">Add to chart</a>
+                </div><br>
+
+              <?php
+              }
             }
           }
 
-          $result_out = mysqli_query($conn, $sql_out);
-          if ($total_price < $buget) {
-            while ($row = $result_out->fetch_assoc()) {
-              $s = $row['files']; ?>
-              <div class='div2'>
-                <style>
-                  .div2 {
-                    border-style: groove;
-                  }
-                </style><img src="<?php echo $s; ?>" alt="HTML5 Icon" style="width:500px;height:500px">
-                <br>Category:
-                <?php echo $row["name"]; ?> <br>Price:
-                <?php echo $row['price']; ?>
-                <br>Size:
-                <?php echo $row['si_ze']; ?>
-                <br>Description:
-                <?php echo $row['description']; ?>
-                <br><a class="btn btn-outline-success my-2 my-sm-1" href="add_chart.php?id=<?php echo $row['id']; ?>"
-                  type="submit">Add to chart</a>
-              </div><br>
-
-            <?php }
-          }
         } else {
           echo "There are no outfits that match your input";
         }
